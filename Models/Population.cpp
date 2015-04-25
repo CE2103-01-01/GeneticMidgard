c756be1f0e884655400aed2fa2158dfcf450d3e5
pablo <paroque28@gmail.com>
2015-04-25 12:59:28 -0600

    Doble Lista

---
+++ Models/Population.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pablo Rodriguez Quesada on 4/25/15.
+//
+
+#include "Population.h"
---
+++ Models/Population.h
@@ -0,0 +1,14 @@
+//
+// Created by Pablo Rodriguez Quesada on 4/25/15.
+//
+
+#ifndef PROJECTMIDGARD_POPULATION_H
+#define PROJECTMIDGARD_POPULATION_H
+
+
+class Population {
+
+};
+
+
+#endif //PROJECTMIDGARD_POPULATION_H
