a31585b7d8d88098542b66f64076dccb00a2aca9
roberto <rbj1504@hotmail.com>
2015-04-27 15:24:27 -0600

    Se inicia el mezclador de cromosomas

--- Models/ChromosomeMixer.cpp
+++ Models/ChromosomeMixer.cpp
@@ -5,12 +5,12 @@
 #include "ChromosomeMixer.h"
 
 /* Mezclador de cromosomas
- * @brief: Recibe la informacion genetica de dos individuos, la mezcla y retorna dos individuos que se accesan
- *         mediante aritmetica de punteros
+ * @brief: Recibe la informacion genetica de dos individuos, la mezcla y retorna dos
+ *         individuos que se accesan mediante aritmetica de punteros
  * @param: Chromosome* fatherGeneticInformation: cromosoma del padre
  * @param: Chromosome* motherGeneticInformation: cromosoma de la madre
  * @return Chromosome*: dos cromosomas hijos, (return) & (return + sizeof(Chromosome))
  */
 Chromosome* ChromosomeMixer::mix(Chromosome* fatherGeneticInformation, Chromosome* motherGeneticInformation){
-    
+
 };
