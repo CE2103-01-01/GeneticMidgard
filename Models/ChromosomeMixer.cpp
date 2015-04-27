7229588d791a334dbb1a53e812de621da1e84449
roberto <rbj1504@hotmail.com>
2015-04-27 15:23:00 -0600

    Se inicia el mezclador de cromosomas

---
+++ Models/ChromosomeMixer.cpp
@@ -0,0 +1,16 @@
+//
+// Created by roberto on 27/04/15.
+//
+
+#include "ChromosomeMixer.h"
+
+/* Mezclador de cromosomas
+ * @brief: Recibe la informacion genetica de dos individuos, la mezcla y retorna dos individuos que se accesan
+ *         mediante aritmetica de punteros
+ * @param: Chromosome* fatherGeneticInformation: cromosoma del padre
+ * @param: Chromosome* motherGeneticInformation: cromosoma de la madre
+ * @return Chromosome*: dos cromosomas hijos, (return) & (return + sizeof(Chromosome))
+ */
+Chromosome* ChromosomeMixer::mix(Chromosome* fatherGeneticInformation, Chromosome* motherGeneticInformation){
+    
+};
---
+++ Models/ChromosomeMixer.h
@@ -0,0 +1,16 @@
+//
+// Created by roberto on 27/04/15.
+//
+
+#ifndef PROJECTMIDGARD_CHROMOSOMEMIXER_H
+#define PROJECTMIDGARD_CHROMOSOMEMIXER_H
+
+#include "Chromosome.h"
+
+class ChromosomeMixer {
+    public:
+        static Chromosome* mix(Chromosome*,Chromosome*);
+};
+
+
+#endif //PROJECTMIDGARD_CHROMOSOMEMIXER_H
