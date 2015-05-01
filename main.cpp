1023362455f5e0c56315141546ec04f5789c2d75
Alex <alexfengli23@gmail.com>
2015-05-01 07:07:45 -0600

    object manager version imcomplete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-#include "Proof/chromosomeMixerProof.h"
+#include <SFML/Graphics.hpp>
 
-int main() {
-    for(struct {int a=0; int b=0;} s; s.a<5 & s.b<5; s.a++, s.b++){
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
+
     return 0;
 }
