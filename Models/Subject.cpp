1543203e1c52c6710b6d51493762f2777d52481b
roberto <rbj1504@hotmail.com>
2015-05-11 16:30:12 -0600

    Managers

--- Models/Subject.cpp
+++ Models/Subject.cpp
@@ -193,10 +193,11 @@ void Subject::start_p_thread(){
     Vector2D positionsVector = Terrain::getRandomFreePosition();
     Terrain::set(positionsVector,*id);
     position = static_cast<int*>(malloc(2 * sizeof(int)));
-    *(position) = 0;
-    *(position + 1) = 0;
-    //Thread message(std::bind(&SocketLogic::getInstance()->createSubject,*id,*(position),*(position+1),*(characteristics+POSITION_OF_RED),
-      //                         *(characteristics+POSITION_OF_GREEN),*(characteristics+POSITION_OF_BLUE)));
+    *(position) = positionsVector.x;
+    *(position + 1) = positionsVector.y;
+    //Thread message(std::bind(&SocketLogic::getInstance()->createSubject,*id,*(position),*(position+1),
+    //                          *(characteristics+POSITION_OF_RED, *(characteristics+POSITION_OF_GREEN),
+    //                          *(characteristics+POSITION_OF_BLUE)));
     //message.launch();
     void* parameters = malloc(sizeof(PThreadParam));
     new(static_cast<PThreadParam*>(parameters)) PThreadParam(this,NULL);
