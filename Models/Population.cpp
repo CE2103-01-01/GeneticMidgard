3ede16c78e4712070aab0c10131f447dd353d7c2
roberto <rbj1504@hotmail.com>
2015-05-27 16:36:45 -0600

    movimiento random

--- Models/Population.cpp
+++ Models/Population.cpp
@@ -110,6 +110,7 @@ void Population::updateFittest(Subject* newMember) {
             *(fittest+i) = tmp;
         }
     }
+    std::cout<< "POBLACION "<<((int)*populationType)<< ", NUEVO PEOR: " << ((*(fittest+2*SUBJECTS_BY_GENERATION-1))->getFitness()) <<std::endl;
 }
 
 /**@brief: busca los muertos
